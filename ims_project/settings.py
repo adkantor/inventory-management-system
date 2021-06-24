ecb53a07bf8270f96150a37b90436ccb5921c91b
Adam Kantor <kantor.a@usa.net>
2021-06-24 16:52:23 +0200

    Environmental variables to docker-compose.yml

--- ims_project/settings.py
+++ ims_project/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-p2nl$7sgbqq$mr$$#6y*#s=n@zzw@ihk1*xi-#!_+hplrvc=6&'
+SECRET_KEY = os.environ.get('SECRET_KEY') #'django-insecure-p2nl$7sgbqq$mr$$#6y*#s=n@zzw@ihk1*xi-#!_+hplrvc=6&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get('DEBUG', default=0))
 
 ALLOWED_HOSTS = []
 
