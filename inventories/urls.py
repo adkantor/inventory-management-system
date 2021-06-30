526f6753bfa01d7257ad53761f87d06e53cfa4d1
Adam Kantor <kantor.a@usa.net>
2021-06-30 16:53:23 +0200

    inventories app views

--- inventories/urls.py
+++ inventories/urls.py
@@ -1,18 +1,31 @@
 from django.urls import path
 
 from .views import (
-    MaterialGroupListView, 
-    MaterialGroupDetailView, 
-    MaterialListView, 
-    MaterialDetailView,
-    TransactionListView,
-    TransactionDetailView)
+    MaterialGroupListView, MaterialGroupDetailView, MaterialGroupCreateView, MaterialGroupUpdateView, MaterialGroupDeleteView, 
+    MaterialListView, MaterialDetailView, MaterialCreateView, MaterialUpdateView, MaterialDeleteView,
+    TransactionListView, TransactionDetailView, GoodsReceiptCreateView, GoodsDispatchCreateView, TransactionUpdateView, TransactionDeleteView
+)
 
 urlpatterns = [
+    # Material Groups
     path('material_groups', MaterialGroupListView.as_view(), name='material_group_list'),
     path('material_groups/<uuid:pk>', MaterialGroupDetailView.as_view(), name='material_group_detail'),
+    path('material_groups/new/', MaterialGroupCreateView.as_view(), name='material_group_new'),
+    path('material_groups/<uuid:pk>/edit/', MaterialGroupUpdateView.as_view(), name='material_group_edit'),
+    path('material_groups/<uuid:pk>/delete/', MaterialGroupDeleteView.as_view(), name='material_group_delete'),
+
+    # Materials
     path('materials', MaterialListView.as_view(), name='material_list'),
     path('materials/<uuid:pk>', MaterialDetailView.as_view(), name='material_detail'),
+    path('materials/new/', MaterialCreateView.as_view(), name='material_new'),
+    path('materials/<uuid:pk>/edit/', MaterialUpdateView.as_view(), name='material_edit'),
+    path('materials/<uuid:pk>/delete/', MaterialDeleteView.as_view(), name='material_delete'),
+
+    # Transactions
     path('transactions', TransactionListView.as_view(), name='transaction_list'),
     path('transactions/<uuid:pk>', TransactionDetailView.as_view(), name='transaction_detail'),
+    path('transactions/goods_receipt_new/', GoodsReceiptCreateView.as_view(), name='goods_receipt_new'),
+    path('transactions/goods_dispatch_new/', GoodsDispatchCreateView.as_view(), name='goods_dispatch_new'),
+    path('transactions/<uuid:pk>/edit/', TransactionUpdateView.as_view(), name='transaction_edit'),
+    path('transactions/<uuid:pk>/delete/', TransactionDeleteView.as_view(), name='transaction_delete'),
 ]
--- inventories/views.py
+++ inventories/views.py
@@ -1,7 +1,11 @@
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from django.contrib.admin import widgets
+
 from .models import MaterialGroup, Material, Transaction
 
 
+# Material Groups
 class MaterialGroupListView(ListView):
     model = MaterialGroup
     context_object_name = 'material_group_list'
@@ -11,6 +15,26 @@ class MaterialGroupDetailView(DetailView):
     model = MaterialGroup
     context_object_name = 'material_group'
     template_name = 'inventories/material_group_detail.html'
+
+class MaterialGroupCreateView(CreateView):
+    model = MaterialGroup
+    fields = ('name',)
+    template_name = 'inventories/material_group_new.html'
+
+class MaterialGroupUpdateView(UpdateView):
+    model = MaterialGroup
+    context_object_name = 'material_group'
+    fields = ('name',)
+    template_name = 'inventories/material_group_edit.html'
+
+class MaterialGroupDeleteView(DeleteView):
+    model = MaterialGroup
+    context_object_name = 'material_group'
+    template_name = 'inventories/material_group_delete.html'
+    success_url = reverse_lazy('material_group_list')
+
+
+# Materials
 
 class MaterialListView(ListView):
     model = Material
@@ -22,6 +46,26 @@ class MaterialDetailView(DetailView):
     context_object_name = 'material'
     template_name = 'inventories/material_detail.html'
 
+class MaterialCreateView(CreateView):
+    model = Material
+    fields = ('name', 'material_group',)
+    template_name = 'inventories/material_new.html'
+
+class MaterialUpdateView(UpdateView):
+    model = Material
+    context_object_name = 'material'
+    fields = ('name', 'material_group',)
+    template_name = 'inventories/material_edit.html'
+
+class MaterialDeleteView(DeleteView):
+    model = Material
+    context_object_name = 'material'
+    template_name = 'inventories/material_delete.html'
+    success_url = reverse_lazy('material_list')
+
+
+# Transactions
+
 class TransactionListView(ListView):
     model = Transaction
     context_object_name = 'transaction_list'
@@ -31,3 +75,53 @@ class TransactionDetailView(DetailView):
     model = Transaction
     context_object_name = 'transaction'
     template_name = 'inventories/transaction_detail.html'
+
+class TransactionDeleteView(DeleteView):
+    model = Transaction
+    context_object_name = 'transaction'
+    template_name = 'inventories/transaction_delete.html'
+    success_url = reverse_lazy('transaction_list')
+
+class GoodsReceiptCreateView(CreateView):
+    model = Transaction
+    fields = ('material', 'transaction_time', 'gross_weight', 'tare_weight',
+              'unit_price', 'notes')
+    template_name = 'inventories/goods_receipt_new.html'
+
+    def get_form(self):
+        form = super(GoodsReceiptCreateView, self).get_form()
+        form.fields['transaction_time'].widget = widgets.AdminSplitDateTime()
+        return form
+
+    def form_valid(self, form):
+        form.instance.transaction_type = Transaction.TYPE_IN
+        return super().form_valid(form)
+
+class GoodsDispatchCreateView(CreateView):
+    model = Transaction
+    fields = ('material', 'transaction_time', 'gross_weight', 'tare_weight',
+              'unit_price', 'notes')
+    template_name = 'inventories/goods_dispatch_new.html'
+
+    def get_form(self):
+        form = super(GoodsDispatchCreateView, self).get_form()
+        form.fields['transaction_time'].widget = widgets.AdminSplitDateTime()
+        return form
+
+    def form_valid(self, form):
+        form.instance.transaction_type = Transaction.TYPE_OUT
+        return super().form_valid(form)   
+
+class TransactionUpdateView(UpdateView):    
+    model = Transaction
+    context_object_name = 'transaction'
+    fields = ('transaction_type', 'material', 'transaction_time', 'gross_weight', 'tare_weight',
+              'unit_price', 'notes')
+    template_name = 'inventories/transaction_edit.html' 
+
+    def get_form(self):
+        form = super(TransactionUpdateView, self).get_form()
+        form.fields['transaction_time'].widget = widgets.AdminSplitDateTime()
+        form.fields['transaction_type'].widget.attrs['readonly'] = True
+        form.fields['transaction_type'].widget.attrs['disabled'] = True
+        return form
