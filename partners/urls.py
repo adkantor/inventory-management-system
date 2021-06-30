4a8fc26349e9f6974a4e173d29e222a1e883d950
Adam Kantor <kantor.a@usa.net>
2021-06-30 14:26:17 +0200

    partners app views

--- partners/urls.py
+++ partners/urls.py
@@ -1,10 +1,30 @@
 from django.urls import path
 
-from .views import VendorListView, VendorDetailView, CustomerListView, CustomerDetailView
+from .views import (
+    VendorListView, 
+    VendorDetailView,
+    VendorCreateView,
+    VendorUpdateView,
+    VendorDeleteView, 
+    CustomerListView, 
+    CustomerDetailView,
+    CustomerCreateView,
+    CustomerUpdateView,
+    CustomerDeleteView,
+)
 
 urlpatterns = [
-    path('vendors', VendorListView.as_view(), name='vendor_list'),
-    path('vendors/<uuid:pk>', VendorDetailView.as_view(), name='vendor_detail'),
-    path('customers', CustomerListView.as_view(), name='customer_list'),
-    path('customers/<uuid:pk>', CustomerDetailView.as_view(), name='customer_detail'),
+    # Vendors
+    path('vendors/', VendorListView.as_view(), name='vendor_list'),
+    path('vendors/<uuid:pk>/', VendorDetailView.as_view(), name='vendor_detail'),
+    path('vendors/new/', VendorCreateView.as_view(), name='vendor_new'),
+    path('vendors/<uuid:pk>/edit/', VendorUpdateView.as_view(), name='vendor_edit'),
+    path('vendors/<uuid:pk>/delete/', VendorDeleteView.as_view(), name='vendor_delete'),
+    
+    # Customers
+    path('customers/', CustomerListView.as_view(), name='customer_list'),
+    path('customers/<uuid:pk>/', CustomerDetailView.as_view(), name='customer_detail'),
+    path('customers/new/', CustomerCreateView.as_view(), name='customer_new'),
+    path('customers/<uuid:pk>/edit/', CustomerUpdateView.as_view(), name='customer_edit'),
+    path('customers/<uuid:pk>/delete/', CustomerDeleteView.as_view(), name='customer_delete'),
 ]
--- partners/views.py
+++ partners/views.py
@@ -1,25 +1,64 @@
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+
 from .models import Vendor, Customer
+
+
+# Vendors
 
 class VendorListView(ListView):
     model = Vendor
     context_object_name = 'vendor_list'
     template_name = 'partners/vendor_list.html'
 
-
 class VendorDetailView(DetailView):
     model = Vendor
     context_object_name = 'vendor'
     template_name = 'partners/vendor_detail.html'
 
+class VendorCreateView(CreateView):
+    model = Vendor
+    fields = ('name', 'country', 'postcode', 'city', 'address', 'tax_number', 'is_private_person', 
+              'contact_name', 'contact_phone', 'contact_email',)
+    template_name = 'partners/vendor_new.html'
+
+class VendorUpdateView(UpdateView):
+    model = Vendor
+    fields = ('name', 'country', 'postcode', 'city', 'address', 'tax_number', 'is_private_person', 
+              'contact_name', 'contact_phone', 'contact_email',)
+    template_name = 'partners/vendor_edit.html'
+
+class VendorDeleteView(DeleteView):
+    model = Vendor
+    template_name = 'partners/vendor_delete.html'
+    success_url = reverse_lazy('vendor_list')
+
+
+# Customers
 
 class CustomerListView(ListView):
     model = Customer
     context_object_name = 'customer_list'
     template_name = 'partners/customer_list.html'
 
-
 class CustomerDetailView(DetailView):
     model = Customer
     context_object_name = 'customer'
     template_name = 'partners/customer_detail.html'
+
+class CustomerCreateView(CreateView):
+    model = Customer
+    fields = ('name', 'country', 'postcode', 'city', 'address', 'tax_number', 'is_private_person', 
+              'contact_name', 'contact_phone', 'contact_email',)
+    template_name = 'partners/customer_new.html'
+
+class CustomerUpdateView(UpdateView):
+    model = Customer
+    fields = ('name', 'country', 'postcode', 'city', 'address', 'tax_number', 'is_private_person', 
+              'contact_name', 'contact_phone', 'contact_email',)
+    template_name = 'partners/customer_edit.html'
+
+class CustomerDeleteView(DeleteView):
+    model = Customer
+    template_name = 'partners/customer_delete.html'
+    success_url = reverse_lazy('customer_list')
