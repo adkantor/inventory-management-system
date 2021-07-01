2edd3039e0da4c55591448f28196cecfd2802475
Adam Kantor <kantor.a@usa.net>
2021-07-01 14:54:51 +0200

    tests for transaction model

--- inventories/tests.py
+++ inventories/tests.py
@@ -1,3 +1,6 @@
+import datetime
+from datetime import timezone
+
 from django.test import TestCase, Client
 from django.urls import reverse
 
@@ -56,3 +59,46 @@ class MaterialTests(TestCase):
         self.assertEqual(no_response.status_code, 404)
         self.assertContains(response, 'alu cooler')
         self.assertTemplateUsed(response, 'inventories/material_detail.html')
+
+
+class TransactionTests(TestCase):
+    
+    def setUp(self):
+        mat_group = MaterialGroup.objects.get_or_create(name = 'aluminium')[0]
+        mat = Material.objects.get_or_create(name='alu cooler', material_group=mat_group)[0]
+        
+        self.transaction = Transaction.objects.create(
+            transaction_type=Transaction.TYPE_IN,
+            material=mat,
+            transaction_time=datetime.date(2021,2,3),
+            gross_weight=10.0,
+            tare_weight=2.0,
+            unit_price=5.0,
+            notes='some notes'
+        )
+ 
+
+    def test_transaction_listing(self):
+        self.assertEqual(f'{self.transaction.transaction_type}', Transaction.TYPE_IN)
+        self.assertEqual(f'{self.transaction.material.name}', 'alu cooler')
+        self.assertEqual(f'{self.transaction.material.material_group.name}', 'aluminium')
+        self.assertEqual(self.transaction.gross_weight, 10.0)
+        self.assertEqual(self.transaction.tare_weight, 2.0)
+        self.assertEqual(self.transaction.unit_price, 5.0)
+        self.assertEqual(f'{self.transaction.notes}', 'some notes')
+        self.assertEqual(self.transaction.net_weight, 8.0)
+        self.assertEqual(self.transaction.net_value, 40.0)
+
+    def test_transaction_list_view(self):
+        response = self.client.get(reverse('transaction_list'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'alu cooler')
+        self.assertTemplateUsed(response, 'inventories/transaction_list.html')
+    
+    def test_transaction_detail_view(self):
+        response = self.client.get(self.transaction.get_absolute_url())
+        no_response = self.client.get('/inventories/transactions/12345/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(no_response.status_code, 404)
+        self.assertContains(response, 'alu cooler')
+        self.assertTemplateUsed(response, 'inventories/transaction_detail.html')
