dc8bf9043e7084cdb01c23e4e2fde57338fbc57e
Adam Kantor <kantor.a@usa.net>
2021-07-30 11:42:22 +0200

    deleted unused templates

--- inventories/urls.py
+++ inventories/urls.py
@@ -3,8 +3,6 @@ from django.urls import path
 from .views import (
     MaterialGroupListView, MaterialGroupDetailView, MaterialGroupCreateView, MaterialGroupUpdateView, MaterialGroupDeleteView, 
     MaterialListView, MaterialDetailView, MaterialCreateView, MaterialUpdateView, MaterialDeleteView,
-    # TransactionListView, TransactionDetailView, TransactionDeleteView, 
-    # GoodsReceiptCreateView, GoodsDispatchCreateView, GoodsReceiptUpdateView, GoodsDispatchUpdateView
 )
 
 urlpatterns = [
@@ -21,13 +19,4 @@ urlpatterns = [
     path('materials/new/', MaterialCreateView.as_view(), name='material_new'),
     path('materials/<uuid:pk>/edit/', MaterialUpdateView.as_view(), name='material_edit'),
     path('materials/<uuid:pk>/delete/', MaterialDeleteView.as_view(), name='material_delete'),
-
-    # Transactions
-    # path('transactions', TransactionListView.as_view(), name='transaction_list'),
-    # path('transactions/<uuid:pk>', TransactionDetailView.as_view(), name='transaction_detail'),
-    # path('transactions/goods_receipts/new/', GoodsReceiptCreateView.as_view(), name='goods_receipt_new'),
-    # path('transactions/goods_dispatches/new/', GoodsDispatchCreateView.as_view(), name='goods_dispatch_new'),
-    # path('transactions/goods_receipts/<uuid:pk>/edit/', GoodsReceiptUpdateView.as_view(), name='goods_receipt_edit'),
-    # path('transactions/goods_dispatches/<uuid:pk>/edit/', GoodsDispatchUpdateView.as_view(), name='goods_dispatch_edit'),
-    # path('transactions/<uuid:pk>/delete/', TransactionDeleteView.as_view(), name='transaction_delete'),
 ]
