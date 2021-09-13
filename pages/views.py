e1212d8aa720b7df3112aafbf4ce5f5adacb72ea
Adam Kantor <kantor.a@usa.net>
2021-09-13 22:50:20 +0200

    permissions for homepage

--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,6 @@
 from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-class HomePageView(TemplateView):
+
+class HomePageView(LoginRequiredMixin, TemplateView):
     template_name = 'home.html'
