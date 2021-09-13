d8c93591a88ed1acc2dfaef952503a3ffa62af37
Adam Kantor <kantor.a@usa.net>
2021-09-13 22:58:02 +0200

    test permissions for pages

--- pages/tests.py
+++ pages/tests.py
@@ -1,29 +1,39 @@
-from django.test import SimpleTestCase
+from django.test import TestCase, Client
 from django.urls import reverse, resolve
+from django.contrib.auth import get_user_model
 
 from .views import HomePageView
 
 
-class HomepageTests(SimpleTestCase):
+class HomepageTests(TestCase):
 
     def setUp(self):
+
+        self.user = get_user_model().objects.create_user(
+            username='authorizeruser', 
+            email='user@email.com', 
+            password='testPass123'
+        )
+
+
+    def test_homepage_view_for_logged_out_user(self):
         url = reverse('home')
-        self.response = self.client.get(url)
+        # log-out user
+        self.client.logout()
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, f'{reverse("account_login")}?next={url}')
+        response = self.client.get(f'{reverse("account_login")}?next={url}')
+        self.assertContains(response, 'Sign In')
 
-    def test_homepage_status_code(self):
-        self.assertEqual(self.response.status_code, 200)
-    
-    def test_homepage_url_name(self):
-        self.assertEqual(self.response.status_code, 200)
-
-    def test_homepage_template(self):
-        self.assertTemplateUsed(self.response, 'home.html')
-
-    def test_homepage_contains_correct_html(self):
-        self.assertContains(self.response, 'Homepage')
-    
-    def test_homepage_does_not_contain_incorrect_html(self):
-        self.assertNotContains(self.response, 'Foo Bar')
+    def test_homepage_view_for_logged_in_user(self):
+        url = reverse('home')
+        # log-in user
+        self.client.login(email='user@email.com', password='testPass123')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Inventory Management System')
+        self.assertTemplateUsed(response, 'home.html')
 
     def test_homepage_url_resolves_homepageview(self):
         view = resolve('/')
