95cb18d8614337053175b35198039539094ba426
Adam Kantor <kantor.a@usa.net>
2021-09-16 14:50:03 +0200

    get DB password from environment

--- ims_project/settings.py
+++ ims_project/settings.py
@@ -99,7 +99,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': 'adam',
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
         'HOST': 'db',
         'PORT': 5432
     }
